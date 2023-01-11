2b3ee427ac275af00472d06a999e3623d28c97b7
Paul Rijneveld <paulrijneveld@gmail.com>
2023-01-11 20:20:17 +0000

    Calculate example score

--- main.cs
+++ main.cs
@@ -5,15 +5,22 @@ using System.Linq;
 
 class Program
 {
+    static string start = "AA";
+    static IList<string> exampleSteps = new [] { "DD", "!CC", "BB", "AA", "II", "JJ", "II", "AA", "DD", "!EE", "FF", "GG", "HH", "GG", "FF", "EE", "DD", "CC" }.ToList();
+    
     public static void Main(string[] args)
     {
-        var valves = ReadValves("Input.txt");
+        var valves = ReadValves("Example.txt", clearClosed: false);
 
         foreach (var valve in valves)
             Console.WriteLine(valve);
+
+        var score = CalculateScore(start, exampleSteps, valves);
+        Console.WriteLine();
+        Console.WriteLine($"Score={score}");
     }
 
-    static List<Valve> ReadValves(string fileName)
+    static List<Valve> ReadValves(string fileName, bool clearClosed)
     {
         List<Valve> allValves = new();
 
@@ -23,7 +30,11 @@ class Program
         foreach (var valve in allValves)
             valve.ConnectNeighbors(allValves);
 
-        return ClearClosedValves(allValves)
+        var result = clearClosed
+            ? ClearClosedValves(allValves)
+            : allValves;
+
+        return result
             .OrderBy(v => v.Name)
             .ToList();
     }
@@ -68,20 +79,75 @@ class Program
 
         return new Tunnel(t1.From, t2.To, t1.Steps + t2.Steps);
     }
+
+    static int CalculateScore(string start, IList<string> steps, IEnumerable<Valve> allValves)
+    {
+        const int maxSteps = 30;
+        int score = 0;
+
+        var curr = FindValve(start, allValves);
+        
+        var numSteps = 0;
+        
+        foreach (var step in steps)
+        {
+            numSteps += 1;
+
+            var shouldOpen = !step.StartsWith("!");            
+            var next = FindValve(step, allValves);            
+            var isOpen = next.IsOpen;
+
+            Console.WriteLine();
+            Console.WriteLine($"== Minute {numSteps} ==");
+            Console.WriteLine($"You move to {step}.");
+            
+            if (!isOpen && shouldOpen)
+            {
+                numSteps += 1;
+                
+                next.IsOpen = true;
+                
+                Console.WriteLine();
+                Console.WriteLine($"== Minute {numSteps} ==");
+                Console.WriteLine($"You open valve {step}.");
+                
+
+                var remainingSteps = maxSteps - numSteps;                
+                if (numSteps <= maxSteps)
+                    score += next.Flow * remainingSteps;
+                else
+                    Console.WriteLine($"Stopped counting: num steps = {numSteps} > max steps = {maxSteps}");
+            }
+            
+            curr = next;            
+        }
+
+        return score;
+    }
+
+    static Valve FindValve(string step, IEnumerable<Valve> allValves)
+    {
+        var name = step.StartsWith("!") ? step.Substring(1) : step;
+        return allValves.FirstOrDefault(v => v.Name == name) ?? throw new Exception($"Valve {name} not found");
+    }
 }
 
 class Valve
 {
     public string Name { get; }
     public int Flow { get; }
-    public List<Tunnel> Neighbors = new();
 
     IEnumerable<string> _neighborValves;
-
+    
+    public List<Tunnel> Neighbors = new();
+    public bool IsOpen { get; set; }
+    
     public Valve(string name, int flow, IEnumerable<string> neighborValves)
     {
         Name = name;
         Flow = flow;
+        
+        IsOpen = Flow == 0; // When the flow is 0 it makes no sense to open the valve, so set to Open from the start
 
         _neighborValves = neighborValves;
     }
@@ -101,6 +167,9 @@ class Valve
 
         return new Valve(name, flow, neighborValveNames);
     }
+
+    public bool ReachableFrom(Valve valve)
+        => valve.Neighbors.Any(t => t.To == this);
 
     public override string ToString()
     {
